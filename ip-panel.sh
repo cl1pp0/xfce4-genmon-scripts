ada2462faa0a90ff9692f4706d925196ad4a1b22
cl1pp0 <cl1pp0>
2020-05-16 16:20:59 +0200

    update ip, add weather

--- ip-panel.sh
+++ ip-panel.sh
@@ -5,15 +5,19 @@
 #readonly IP6ADDR="$(ip -family inet6 addr show dev enp3s0 | grep inet6.*temporary | awk -F"[/ ]+" '/inet6 / {print $3}')"
 
 # with net mask
-readonly IP4ADDR="$(ip -f inet addr show dev enp3s0 | awk '/inet / { print $2 }')"
+readonly IP4ADDR="$(ip -f inet addr show dev enp0s25 | awk '/inet / { print $2 }')"
+readonly IP4ADDR2="$(ip -f inet addr show dev wlp3s0 | awk '/inet / { print $2 }')"
 readonly IP6ADDR="$(ip -f inet6 a | grep inet6.*temporary | awk '/inet6 / { print $2 }')"
 
 readonly HOST="$(uname -n)"
+readonly UPTIME="$(uptime | awk '{print $3}' | cut -f1 -d",")"
 
 INFO="<txt>"
-INFO+="<span fgcolor='lightblue'>${HOST} </span>"
-INFO+="<span fgcolor='yellow'>${IP4ADDR} </span>"
-INFO+="<span fgcolor='lightgreen'>${IP6ADDR}</span>"
+#INFO+="<span fgcolor='lightblue'>${HOST} </span>"
+INFO+="<span fgcolor='gold'>${IP4ADDR} </span>"
+INFO+="<span fgcolor='gold'>${IP4ADDR2} </span>"
+INFO+="<span fgcolor='yellowgreen'>${IP6ADDR} </span>"
+INFO+="<span fgcolor='salmon'> up ${UPTIME}</span>"
 INFO+="</txt>"
 
 TOOL_INFO="<tool>"
---
+++ weather-panel.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+readonly WHEATHER="$(curl -s wttr.in/?format="%c+%t+%h+%w")"
+readonly WHEATHER_TOOL="$(curl -s wttr.in/?format="%l:+%c+%t+%h+%p+%P+%S+%s")"
+
+INFO="<txt>"
+INFO+="<span fgcolor='lightblue'>${WHEATHER}</span>"
+INFO+="</txt>"
+
+TOOL_INFO="<tool>"
+TOOL_INFO+="${WHEATHER_TOOL}"
+TOOL_INFO+="</tool>"
+
+echo -e "${INFO}"
+echo -e "${TOOL_INFO}"
+
